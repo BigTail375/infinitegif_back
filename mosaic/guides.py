d2ef9f74ae7a4476424fc5a3409fde3883ac6a28
BigTail <bigtail43118@gmail.com>
2024-12-04 14:31:06 +0900

    update mosaic

--- mosaic/guides.py
+++ mosaic/guides.py
@@ -19,44 +19,38 @@ mpl.rcParams['figure.dpi'] = 300
 def pixellines_to_ordered_points(matrix, half_tile):
     # break guidelines into chains and order the pixel for all chain
 
-    # import cv2
-    # chains3 = []
-
-    matrix = sk.morphology.skeletonize(matrix) # nicer lines, better results
-    matrix_labeled, chain_count = label(matrix, structure=[[1,1,1], [1,1,1], [1,1,1]]) # find chains
+    matrix = sk.morphology.skeletonize(matrix)  # nicer lines, better results
+    matrix_labeled, chain_count = label(matrix, structure=[[1, 1, 1], [1, 1, 1], [1, 1, 1]])  # find chains
     chains = []
-    for i_chain in range(1,chain_count):
+    for i_chain in range(1, chain_count):
         pixel = copy.deepcopy(matrix_labeled)
-        pixel[pixel!=i_chain] = 0
-        
-        # alternative using openCV results results in closed chains (might be better), but a few chains are missing
-        # hierarchy,contours = cv2.findContours(pixel.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        # for h in hierarchy:
-        #     h2 = h.reshape((-1,2))
-        #     h3 = [list(xy)[::-1] for xy in h2]
-        # if len(h3)>3:
-        #     chains3 += [ h3 ]
-        
-        
+        pixel[pixel != i_chain] = 0
+
         while True:
-            points = np.argwhere(pixel!=0)
-            if len(points)==0: break
-            x,y = points[0] # set starting point
+            try:
+                points = np.argwhere(pixel != 0)
+            except Exception as e:
+                print(f"Error finding points in pixel array: {e}")
+                break
+
+            if len(points) == 0:
+                break
+            x, y = points[0]  # set starting point
             done = False
             subchain = []
             while not done:
-                subchain += [[x,y]]
-                pixel[x,y] = 0
+                subchain += [[x, y]]
+                pixel[x, y] = 0
                 done = True
-                for dx,dy in [(+1,0),(-1,0),(+1,-1),(-1,+1),(0,-1,),(0,+1),(-1,-1),(+1,+1)]:
-                    if x+dx>=0 and x+dx<pixel.shape[0] and y+dy>=0 and y+dy<pixel.shape[1]: # prüfen ob im Bild drin
-                        if pixel[x+dx,y+dy]>0: # check for pixel here
-                            x,y = x+dx, y+dy # if yes, jump here
-                            done = False # tell the middle loop that the chain is not finished
-                            break # break inner loop
-            if len(subchain)>half_tile//2:
+                for dx, dy in [(+1, 0), (-1, 0), (+1, -1), (-1, +1), (0, -1,), (0, +1), (-1, -1), (+1, +1)]:
+                    if x + dx >= 0 and x + dx < pixel.shape[0] and y + dy >= 0 and y + dy < pixel.shape[1]:  # check if within image
+                        if pixel[x + dx, y + dy] > 0:  # check for pixel here
+                            x, y = x + dx, y + dy  # if yes, jump here
+                            done = False  # tell the middle loop that the chain is not finished
+                            break  # break inner loop
+            if len(subchain) > half_tile // 2:
                 chains += [subchain]
-   
+
     return chains
 
 
